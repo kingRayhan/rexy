157e779249f708817431f5485a8cd3ae0aef4033
kingrayhan <rayhan095@gmail.com>
2022-07-12 13:46:44 +0600

    :test_tube: TEST: authService.logout

--- src/api/auth/auth.service.spec.ts
+++ src/api/auth/auth.service.spec.ts
@@ -1,5 +1,6 @@
 import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { decode as decodeJWT } from 'jsonwebtoken';
 import { ReturnModelType } from '@typegoose/typegoose';
 import { getModelToken } from 'nestjs-typegoose';
 import configs from '../../app/config';
@@ -48,123 +49,159 @@ describe('AuthService', () => {
     expect(service).toBeDefined();
   });
 
-  it('authService.register -> Register a new user', async () => {
-    await userModel.deleteMany({});
-    const payload: AuthRegisterDTO = {
-      email: 'john@gmail.com',
-      password: '123456',
-      name: 'John Doe',
-      username: 'johndoe',
-    };
-    service.register(payload).then((user) => {
-      expect(user).toBeDefined();
-      expect(user.email).toBe(payload.email);
-      expect(user.name).toBe(payload.name);
-      expect(user.username).toBe(payload.username);
-      expect(user.password).not.toBe(payload.password);
-    });
-    await userModel.deleteMany({});
-  });
-
-  it('authService.register -> throw 403 if username exists', async () => {
-    await userModel.deleteMany({});
-    const payload: AuthRegisterDTO = {
-      name: 'John Doe1',
-      username: 'johndoe1',
-      email: 'john1@gmail.com',
-      password: '123456',
-    };
-    // create a user document
-    await userModel.create(payload);
-
-    service.register({ ...payload, email: 'xxx1@xx.com' }).catch((err) => {
-      expect(err.status).toBe(403);
-      expect(err.message).toBe(AppMessage.USERNAME_ALREADY_EXISTS);
-    });
-    await userModel.deleteMany({});
-  });
-
-  it('authService.register -> throw 403 if email exists', async () => {
-    await userModel.deleteMany({});
-    const payload: AuthRegisterDTO = {
-      name: 'John Doed',
-      username: 'johndoe1',
-      email: 'johdn@gmail.com',
-      password: '123456',
-    };
-    // create a user document
-    await userModel.create(payload);
-
-    service.register({ ...payload, username: 'xxx1' }).catch((err) => {
-      expect(err.status).toBe(403);
-      expect(err.message).toBe(AppMessage.EMAIL_ALREADY_EXISTS);
-    });
-    await userModel.deleteMany({});
-  });
-
-  it('authService.login -> Get access and refresh token using username and password', async () => {
-    await userModel.deleteMany({});
-    const payload: AuthRegisterDTO = {
-      email: 'john@gmail.com',
-      password: '123456',
-      name: 'John Doe',
-      username: 'johndoe',
-    };
-    await userModel.create(payload);
-
-    service
-      .login({
-        user: payload.username,
+  describe('authService.register', () => {
+    it('authService.register -> Register a new user', async () => {
+      await userModel.deleteMany({});
+      const payload: AuthRegisterDTO = {
+        email: 'john@gmail.com',
         password: '123456',
-      })
-      .then((res) => {
+        name: 'John Doe',
+        username: 'johndoe',
+      };
+      service.register(payload).then((user) => {
+        expect(user).toBeDefined();
+        expect(user.email).toBe(payload.email);
+        expect(user.name).toBe(payload.name);
+        expect(user.username).toBe(payload.username);
+        expect(user.password).not.toBe(payload.password);
+      });
+      await userModel.deleteMany({});
+    });
+
+    it('authService.register -> throw 403 if username exists', async () => {
+      await userModel.deleteMany({});
+      const payload: AuthRegisterDTO = {
+        name: 'John Doe1',
+        username: 'johndoe1',
+        email: 'john1@gmail.com',
+        password: '123456',
+      };
+      // create a user document
+      await userModel.create(payload);
+
+      service.register({ ...payload, email: 'xxx1@xx.com' }).catch((err) => {
+        expect(err.status).toBe(403);
+        expect(err.message).toBe(AppMessage.USERNAME_ALREADY_EXISTS);
+      });
+      await userModel.deleteMany({});
+    });
+
+    it('authService.register -> throw 403 if email exists', async () => {
+      await userModel.deleteMany({});
+      const payload: AuthRegisterDTO = {
+        name: 'John Doed',
+        username: 'johndoe1',
+        email: 'johdn@gmail.com',
+        password: '123456',
+      };
+      // create a user document
+      await userModel.create(payload);
+
+      service.register({ ...payload, username: 'xxx1' }).catch((err) => {
+        expect(err.status).toBe(403);
+        expect(err.message).toBe(AppMessage.EMAIL_ALREADY_EXISTS);
+      });
+      await userModel.deleteMany({});
+    });
+  });
+
+  describe('authService.login', () => {
+    it('authService.login -> Get access and refresh token using username and password', async () => {
+      await userModel.deleteMany({});
+      const payload: AuthRegisterDTO = {
+        email: 'john@gmail.com',
+        password: '123456',
+        name: 'John Doe',
+        username: 'johndoe',
+      };
+      await userModel.create(payload);
+
+      service
+        .login({
+          user: payload.username,
+          password: '123456',
+        })
+        .then((res) => {
+          expect(res).toBeDefined();
+          expect(res.accessToken).toBeDefined();
+          expect(res.refreshToken).toBeDefined();
+        });
+      await userModel.deleteMany({});
+    });
+
+    it('authService.login -> get 403 for invalid username', async () => {
+      await userModel.deleteMany({});
+      const payload: AuthRegisterDTO = {
+        email: 'john@gmail.com',
+        password: '123456',
+        name: 'John Doe',
+        username: 'johndoe',
+      };
+      await userModel.create(payload);
+
+      service
+        .login({
+          user: 'wrong-username',
+          password: payload.password,
+        })
+        .catch((err) => {
+          expect(err.status).toBe(403);
+          expect(err.message).toBe(AppMessage.INVALID_CREDENTIALS);
+        });
+      await userModel.deleteMany({});
+    });
+
+    it('authService.login -> get 403 for invalid password', async () => {
+      await userModel.deleteMany({});
+      const payload: AuthRegisterDTO = {
+        email: 'john@gmail.com',
+        password: '123456',
+        name: 'John Doe',
+        username: 'johndoe',
+      };
+      await userModel.create(payload);
+      service
+        .login({
+          user: payload.username,
+          password: 'wrong---password',
+        })
+        .catch((err) => {
+          expect(err.status).toBe(403);
+          expect(err.message).toBe(AppMessage.INVALID_CREDENTIALS);
+        });
+      await userModel.deleteMany({});
+    });
+  });
+
+  describe('authService.logout', () => {
+    it('authService.logout -> Logout using session id', async () => {
+      await userModel.deleteMany({});
+      const payload: AuthRegisterDTO = {
+        email: 'john@gmail.com',
+        password: '123456',
+        name: 'John Doe',
+        username: 'johndoe',
+      };
+      await userModel.create(payload);
+
+      service
+        .login({ user: payload.username, password: payload.password })
+        .then(({ accessToken }) => {
+          const decoded = decodeJWT(accessToken);
+          service.logout(decoded['session_id']).then((res) => {
+            expect(res).toBeDefined();
+            expect(res.deletedCount).toBe(1);
+            expect(res.acknowledged).toBe(true);
+          });
+        });
+    });
+
+    it('authService.logout -> get deletedCount = 0 for wrong session_id', async () => {
+      service.logout('62cd25d19278aeb09e0eab9f').then((res) => {
         expect(res).toBeDefined();
-        expect(res.accessToken).toBeDefined();
-        expect(res.refreshToken).toBeDefined();
+        expect(res.acknowledged).toBe(true);
+        expect(res.deletedCount).toBe(0);
       });
-    await userModel.deleteMany({});
-  });
-
-  it('authService.login -> get 403 for invalid username', async () => {
-    await userModel.deleteMany({});
-    const payload: AuthRegisterDTO = {
-      email: 'john@gmail.com',
-      password: '123456',
-      name: 'John Doe',
-      username: 'johndoe',
-    };
-    await userModel.create(payload);
-
-    service
-      .login({
-        user: 'wrong-username',
-        password: payload.password,
-      })
-      .catch((err) => {
-        expect(err.status).toBe(403);
-        expect(err.message).toBe(AppMessage.INVALID_CREDENTIALS);
-      });
-    await userModel.deleteMany({});
-  });
-
-  it('authService.login -> get 403 for invalid password', async () => {
-    await userModel.deleteMany({});
-    const payload: AuthRegisterDTO = {
-      email: 'john@gmail.com',
-      password: '123456',
-      name: 'John Doe',
-      username: 'johndoe',
-    };
-    await userModel.create(payload);
-    service
-      .login({
-        user: payload.username,
-        password: 'wrong---password',
-      })
-      .catch((err) => {
-        expect(err.status).toBe(403);
-        expect(err.message).toBe(AppMessage.INVALID_CREDENTIALS);
-      });
-    await userModel.deleteMany({});
+    });
   });
 });
