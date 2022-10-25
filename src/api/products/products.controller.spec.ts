d2770b61ce14c5fa444642f688510190505e0e31
kingrayhan <rayhan095@gmail.com>
2022-10-26 00:09:54 +0600

    Product Service Test

--- src/api/products/products.controller.spec.ts
+++ src/api/products/products.controller.spec.ts
@@ -1,20 +1,55 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ProductsController } from './products.controller';
-import { ProductsService } from './products.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { ProductsController } from "./products.controller";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import { ReturnModelType } from "@typegoose/typegoose";
+import { ConfigModule } from "@nestjs/config";
+import configs from "@/app/config";
+import { TestDatabaseModule } from "@/shared/test-database/test-database.module";
+import { getModelToken, TypegooseModule } from "nestjs-typegoose";
+import validationOptions from "@/app/utils/validation-options";
+import { Product } from "@/api/products/entities/product.entity";
+import { ProductsService } from "@/api/products/products.service";
+import { ProductEventListener } from "@/api/products/products.event-listener";
+import * as request from "supertest";
 
-describe('ProductsController', () => {
+describe("ProductsController", () => {
+  let app: INestApplication;
   let controller: ProductsController;
+  let productModel: ReturnModelType<typeof Product>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          load: configs
+        }),
+        TestDatabaseModule,
+        TypegooseModule.forFeature([Product])
+        // ---
+      ],
       controllers: [ProductsController],
-      providers: [ProductsService],
+      providers: [
+        ProductsService, ProductEventListener
+      ]
     }).compile();
-
     controller = module.get<ProductsController>(ProductsController);
+    productModel = module.get<ReturnModelType<typeof Product>>(getModelToken("Product"));
+
+    app = module.createNestApplication();
+    app.enableShutdownHooks();
+    app.useGlobalPipes(new ValidationPipe(validationOptions));
+    await app.init();
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  describe("GET /products", () => {
+    it("should create a product", async () => {
+      const response = await request(app.getHttpServer())
+        .post("/products")
+        .send();
+    });
   });
 });
