a86bc5d341a02ed00811bafcf29e65747bd9b5ef
kingrayhan <rayhan095@gmail.com>
2022-10-26 00:23:35 +0600

    product controller test: POST /products

--- src/api/products/products.controller.spec.ts
+++ src/api/products/products.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { ProductsController } from "./products.controller";
-import { INestApplication, ValidationPipe } from "@nestjs/common";
+import { HttpStatus, INestApplication, ValidationPipe } from "@nestjs/common";
 import { ReturnModelType } from "@typegoose/typegoose";
 import { ConfigModule } from "@nestjs/config";
 import configs from "@/app/config";
@@ -11,6 +11,76 @@ import { Product } from "@/api/products/entities/product.entity";
 import { ProductsService } from "@/api/products/products.service";
 import { ProductEventListener } from "@/api/products/products.event-listener";
 import * as request from "supertest";
+
+
+const demoProducts: Product[] = [
+  {
+    name: "Test Product 1",
+    price: 100,
+    availability: true,
+    code: "test-product-1",
+    discount_rate: 0,
+    durability: 0,
+    max_durability: 0,
+    mileage: 0,
+    minimum_rent_period: 0,
+    needing_repair: false,
+    type: "plain"
+  },
+  {
+    name: "Test Product 2",
+    price: 100,
+    availability: true,
+    code: "test-product-2",
+    discount_rate: 0,
+    durability: 0,
+    max_durability: 0,
+    mileage: 0,
+    minimum_rent_period: 0,
+    needing_repair: false,
+    type: "plain"
+  },
+  {
+    name: "Test Product 3",
+    price: 100,
+    availability: true,
+    code: "test-product-3",
+    discount_rate: 0,
+    durability: 0,
+    max_durability: 0,
+    mileage: 0,
+    minimum_rent_period: 0,
+    needing_repair: false,
+    type: "plain"
+  },
+  {
+    name: "Test Product 4",
+    price: 100,
+    availability: true,
+    code: "test-product-4",
+    discount_rate: 0,
+    durability: 0,
+    max_durability: 0,
+    mileage: 0,
+    minimum_rent_period: 0,
+    needing_repair: false,
+    type: "plain"
+  },
+  {
+    name: "Test Product 5",
+    price: 100,
+    availability: true,
+    code: "test-product-5",
+    discount_rate: 0,
+    durability: 0,
+    max_durability: 0,
+    mileage: 0,
+    minimum_rent_period: 0,
+    needing_repair: false,
+    type: "plain"
+  }
+];
+
 
 describe("ProductsController", () => {
   let app: INestApplication;
@@ -46,10 +116,29 @@ describe("ProductsController", () => {
   });
 
   describe("GET /products", () => {
-    it("should create a product", async () => {
+    it("Product list route defined", () => {
+      expect(controller.findAll).toBeDefined();
+    });
+
+
+    it("Get list of products with pagination", async () => {
+      await productModel.deleteMany();
+      await productModel.create(demoProducts);
       const response = await request(app.getHttpServer())
-        .post("/products")
-        .send();
+        .get("/products");
+      expect(response.status).toEqual(HttpStatus.OK);
+      expect(response.body.data.contents).toBeDefined();
+      expect(response.body.data.meta.resourceCount).toBe(demoProducts.length);
+    });
+  });
+
+  describe("POST /products", () => {
+    it("⛔ Throw 401: For unauthenticated request", async () => {
+      const response = await request(app.getHttpServer())
+        .post("/products");
+
+      expect(response.status).toBe(HttpStatus.UNAUTHORIZED);
+      expect(response.body).toHaveProperty('message');
     });
   });
 });
