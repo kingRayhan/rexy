98dec5389a3528b5d471b554d07c0b619f112392
kingrayhan <rayhan095@gmail.com>
2022-10-26 22:46:40 +0600

    test: SessionService

--- src/api/user/user.controller.spec.ts
+++ src/api/user/user.controller.spec.ts
@@ -1,32 +1,38 @@
-import { TestDatabaseModule } from './../../shared/test-database/test-database.module';
-import configs from '../../app/config';
 import { Test, TestingModule } from '@nestjs/testing';
-import { TypegooseModule } from 'nestjs-typegoose';
-import { User } from './entities/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { ConfigModule } from '@nestjs/config';
+import { TestScaffoldModule } from '@/shared/test-scaffold/test-scaffold.module';
+import { TestScaffoldService } from '@/shared/test-scaffold/test-scaffold.service';
+import * as request from 'supertest';
+import { INestApplication } from '@nestjs/common';
 
 describe('UserController', () => {
+  let app: INestApplication;
   let controller: UserController;
+  let testScaffoldService: TestScaffoldService;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot({
-          load: configs,
-        }),
-        TestDatabaseModule,
-        TypegooseModule.forFeature([User]),
-      ],
+      imports: [TestScaffoldModule],
       controllers: [UserController],
       providers: [UserService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    testScaffoldService = module.get<TestScaffoldService>(TestScaffoldService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('GET /user', () => {
+    it('Get authenticated user using valid access token', async () => {
+      const { token } = await testScaffoldService.createTestUserAndToken();
+
+      const refreshAgain = await request(app.getHttpServer())
+        .post('/auth/refresh')
+        .set('Authorization', `Bearer ${token.accessToken}`);
+    });
   });
 });
